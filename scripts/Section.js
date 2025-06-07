6dc48c8a8b043f538190357e42f9318cb171b153
SaraiVelasquez <saraivelasquez58@gmail.com>
2025-06-07 02:55:06 -0500

    clase_section_creada

---
+++ scripts/Section.js
@@ -0,0 +1,25 @@
+export default class Section{
+    constructor({items, renderer}, containerSelector){
+        this._items = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(containerSelector);
+    }
+
+
+renderItems() {
+    this._items.forEach(item => {
+      const element = this._renderer(item);
+      this.addItem(element);
+    });
+  }
+
+
+addItem(element, prepend = false) {
+    if (prepend) {
+      this._container.prepend(element);
+    } else {
+      this._container.append(element);
+    }
+  }
+}
+
